716325b2facd2a943668e106ad35f9911c636948
tauqeer-ahmad-gh <tauqeer@agaze.in>
2021-09-05 18:50:53 +0530

    logical operators

--- basics-i/assignment.js
+++ basics-i/assignment.js
@@ -105,11 +105,99 @@ console.log(typeof typeof null); // string
 // console.log({ x, y });
 
 // string literals
-const firstName = 'Tauqeer';
-const lastName = 'Ahmad';
-const birthYear = 1993;
+// const firstName = 'Tauqeer';
+// const lastName = 'Ahmad';
+// const birthYear = 1993;
 
-const message = `I'm  ${firstName} ${lastName} and I am 
-  ${2021 - birthYear} years old.`;
+// const message = `I'm  ${firstName} ${lastName} and I am
+//   ${2021 - birthYear} years old.`;
 
-console.log(message);
+// console.log(message);
+
+// Convert to Number
+// const value = 0;
+// const valueAsNumber = +value; // + converts into number
+// const valueAsNumber = Number(value); // Number(value) into number if it can be converted
+// console.log({ value, valueAsNumber });
+
+// null ===> 0
+// true ===> 1
+// false ===> 0
+// undefined ===> NaN
+
+// Convert to String
+// const value = 0;
+// const valueAsString = String(value); // String(value) into string if it can be converted
+// console.log({ value, valueAsString });
+
+// null ===> 'null'
+// true ===> 'true'
+// false ===> 'false'
+// undefined ===> 'undefined'
+
+// Convert to Boolean
+// Truthy and Falsy values
+// Truthy
+
+// Falsy
+// 0
+// '' => empty string
+// undefined
+// null
+// NaN
+
+// const value = undefined;
+// const valueAsBoolean = Boolean(value); // Boolean(value) into boolean if it can be converted
+// console.log({ value, valueAsBoolean });
+
+// null ===> false
+// true ===> true
+// false ===> false
+// undefined ===> false
+// '' ===> false
+// 0 ===> false
+// {} ===> true
+
+// =======================================
+
+// Type Coercion
+// console.log('20' - 28 + '15'); // -815
+// console.log('20' + 28 * '5'); // 20140
+// console.log('20' - 28 * '5'); // -120
+// console.log('20' > 28 * '10'); // false
+// console.log('20' < 28 - '400'); // false
+// console.log('20' > 28); // false
+// if the operator can only be applicable after type changing and it is possible, then it is done otherwise changes to string
+
+// let num = '1' + 1; // converts to string '11'
+// num = num - 1; // - operator changes '11' to number 11
+// console.log(num); // 10
+
+// let height; // undefined is falsy value
+// let height = 0; // 0 falsy value
+
+// if (height) {
+//   console.log('you have height of ' + height);
+// } else {
+//   console.log('height is not defined'); // bug; height is defined but is 0. If we only look for undefined, we will run into problem
+// }
+
+// logical operators
+// || OR operator
+// && AND operator
+
+// if (height || height === 0) {
+//   console.log('you have height of ' + height);
+// } else {
+//   console.log('height is not defined'); // bug; height is defined but is 0. If we only look for undefined, we will run into problem
+// }
+
+// true || true => true
+// true || false => true
+// false || true => true
+// false || false => false
+
+// true && true => true
+// true && false => false
+// false && true => false
+// false && false => false
